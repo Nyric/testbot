c110928e82b4db0c5b9c5ced06da4ce515930e65
NyricEvans <71023938+NyricEvans@users.noreply.github.com>
2022-11-22 00:40:22 -0600

    Config Update

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
-    consumer_key:         'mt6N6lIkbFZH2E51sJPBiaiCT',
-    consumer_secret:      'JkJmc6dGrzToiuugxWKuDKgaEjfPsc779D8vOQ7ngYvtBmR16Y',
-    access_token:         '1443757711649349639-uefeMYVbfVpzNlt2wBiTgtavuUIdOH',
-    access_token_secret:  'OZHbd0FiI8zZN9TI2Tr39fW0q8eok8gHJHjLHTC8186j3'
+    consumer_key:         '4FWbdE8DwCm9c6dj8D30RNrHo',
+    consumer_secret:      'eZCidr5aMaDPHpTlzMFrXZobSLEqZShdQZuB8UPFpme0KH47st',
+    access_token:         '1536479591619039233-enicAEVojWkEJRYrXScFEmk1zYQNqV',
+    access_token_secret:  'iduEDEK4VAXetSRM8sYBAGJ7HLyKp9xqA5spd66wE6yDf'
 }
 
 module.exports = config;
